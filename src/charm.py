9bff44ef0f88c6345183c9596b2cc832c92c2ddb
karimsye <syed.karim@canonical.com>
2021-07-09 00:39:11 -0400

    Implemented basic config-change that handles install hook as well

--- src/charm.py
+++ src/charm.py
@@ -18,6 +18,7 @@ from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
 from ops.model import ActiveStatus
+from ops.pebble import ConnectionError
 
 logger = logging.getLogger(__name__)
 
@@ -29,17 +30,18 @@ class FluentdOperatorCharm(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.fluentd_pebble_ready, self._on_fluentd_pebble_ready)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
         # self.framework.observe(self.on.fortune_action, self._on_fortune_action)
         self._stored.set_default(things=[])
 
-    def _on_fluentd_pebble_ready(self, event):
-        """Define and start a workload using the Pebble API."""
-        # Get a reference the container attribute on the PebbleReadyEvent
-        container = event.workload
-        # Define an initial Pebble layer configuration
-        pebble_layer = {
+    def _on_config_changed(self, event):
+        """Handle the config changed event."""
+        # return
+        # Get a reference to the container so we can manipulate it
+        container = self.unit.get_container("fluentd")
+        # Create a new config layer - specify 'override: merge' in the 'fluentd'
+        # service definition to overlay with existing layer
+        layer = {
             "summary": "fluentd layer",
             "description": "pebble config layer for fluent/fluentd",
             "services": {
@@ -51,41 +53,37 @@ class FluentdOperatorCharm(CharmBase):
                     "startup": "enabled",
                     "environment": {"FLUENT_ELASTICSEARCH_HOST": self.model.config["elasticsearch-hostname"],
                                     "FLUENT_ELASTICSEARCH_PORT": self.model.config["elasticsearch-port"],
-                                    "FLUENT_ELASTICSEARCH_SSL_VERIFY": "false"},
+                                    "FLUENT_ELASTICSEARCH_SSL_VERIFY": "false",
+                                    "FLUENTD_SYSTEMD_CONF": "disable"
+                                   },
                 }
             },
         }
-        # Add intial Pebble config layer using the Pebble API
-        container.add_layer("fluentd", pebble_layer, combine=True)
-        # Autostart any services that were defined with startup: enabled
-        container.autostart()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
+        try:
+            # Get the current config
+            services = container.get_plan().to_dict().get("services", {})
+        except ConnectionError:
+            # Since this is a config-changed handler and that hook can execute
+            # before pebble is ready, we may get a connection error here. Let's
+            # defer the event, meaning it will be retried the next time any
+            # hook is executed. This method will be rerun when that condition
+            #  is met (because of `event.defer()`), and so the `get_container` 
+            # call will succeed and we'll continue to the subsequent steps.
+            event.defer()
+            return
+        # Check if there are any changes to services
+        if services != layer["services"]:
+            # Changes were made, add the new layer
+            container.add_layer("fluentd", layer, combine=True)
+            logging.info("Added updated layer 'fluentd' to Pebble plan")
+            # Stop the service if it is already running
+            if container.get_service("fluentd").is_running():
+                container.stop("fluentd")
+            # Restart it and report a new status to Juju
+            container.start("fluentd")
+            logging.info("Restarted fluentd service")
+        # All is well, set an ActiveStatus
         self.unit.status = ActiveStatus()
-
-    def _on_config_changed(self, _):
-        """Just an example to show how to deal with changed configuration."""
-        return
-        current = self.config["thing"]
-        if current not in self._stored.things:
-            logger.debug("found a new thing: %r", current)
-            self._stored.things.append(current)
-
-    # def _on_fortune_action(self, event):
-    #     """Just an example to show how to receive actions.
-
-    #     TEMPLATE-TODO: change this example to suit your needs.
-    #     If you don't need to handle actions, you can remove this method,
-    #     the hook created in __init__.py for it, the corresponding test,
-    #     and the actions.py file.
-
-    #     Learn more about actions at https://juju.is/docs/sdk/actions
-    #     """
-    #     fail = event.params["fail"]
-    #     if fail:
-    #         event.fail(fail)
-    #     else:
-    #         event.set_results({"fortune": "A bug in the code is worth two in the documentation."})
 
 
 if __name__ == "__main__":
